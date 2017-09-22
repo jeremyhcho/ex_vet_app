efaaf0c05f2425dd04f971e554a342ef08b23407
Unknown <hyung.cho@hyfn.com>
2017-09-21 23:21:48 -0700

    Changes response concern to find default serializer

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,13 +1,20 @@
 module Response
   def json_response(object, status = :ok, opts = {})
-    if opts[:each_serializer]
+    if object.is_a? ActiveRecord::Relation
       object = object.each_with_object([]) do |obj, building_arr|
-        building_arr << opts[:each_serializer].new(obj).serializable_hash
+        building_arr << serialize(obj)
       end
-    elsif opts[:serializer]
-      object = opts[:serializer].new(object).serializable_hash
+    elsif object.is_a? ActiveRecord::Base
+      object = serialize(object)
     end
 
     render json: object, status: status
+  end
+
+  def serialize(object)
+    "#{object.class.to_s.pluralize}::ShowSerializer"
+      .constantize
+      .new(object)
+      .serializable_hash
   end
 end
